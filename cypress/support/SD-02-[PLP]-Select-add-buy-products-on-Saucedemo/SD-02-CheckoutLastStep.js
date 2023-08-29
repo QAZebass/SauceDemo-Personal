b4a7db23997a64ab315e8dbcdd961403e952c0ef
QAZebass <ze.basszender@gmail.com>
2023-08-28 23:11:37 -0300

    SD-02| Test case 2 finished

--- cypress/support/SD-02-[PLP]-Select-add-buy-products-on-Saucedemo/SD-02-CheckoutLastStep.js
+++ cypress/support/SD-02-[PLP]-Select-add-buy-products-on-Saucedemo/SD-02-CheckoutLastStep.js
@@ -1,6 +1,12 @@
 class CheckoutLastStep{
     get={
-        
+        checkoutTitle:()=> cy.get('[class="title"]'),
+        thanksSign:()=> cy.get('[class="complete-header"]'),
+        orderDispatched:()=> cy.get('[class="complete-text"]'),
+        backHomeButton:()=> cy.get('[data-test="back-to-products"]')
+    }
+    clickBackToHome(){
+        this.get.backHomeButton().click()
     }
 }
 export const checkoutlaststep = new CheckoutLastStep()
